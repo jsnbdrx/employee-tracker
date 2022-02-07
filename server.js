119ffcc53ab63383b179c4bc5136cb8aa7e9d770
Jason Boudreaux <js.bdrx@gmail.com>
2022-02-06 22:33:03 -0600

    created initial database connection in server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql2');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'yQ8szawPc~xrKTB',
+      database: 'employee_tracker'
+    },
+    console.log('Connected to the Employee Tracker database.')
+  );
+
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
+    });
+  });
+
+  // Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
